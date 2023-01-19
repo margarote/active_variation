b7adc5929ea1462f1801a0fc08952c1e77f541a5
margarote <joaovitortur1@gmail.com>
2023-01-19 09:36:58 -0300

    add shimmer

---
+++ lib/core/widgets/loading_shimmer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:variacao/core/colors/colors_app.dart';
+
+class LoadingShimmer extends StatelessWidget {
+  final Widget child;
+  const LoadingShimmer({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: ColorsApp.shimmer.first,
+      highlightColor: ColorsApp.shimmer.last,
+      child: child,
+    );
+  }
+}
