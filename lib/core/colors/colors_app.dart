b5b1839a93688f97d355120caba32698a4f61f72
margarote <joaovitortur1@gmail.com>
2023-01-19 09:35:03 -0300

    add colors

---
+++ lib/core/colors/colors_app.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static Color background = const Color.fromARGB(255, 255, 252, 252);
+  static List<Color> shimmer = [Colors.grey.shade400, Colors.grey.shade100];
+}
